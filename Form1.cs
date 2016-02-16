8def092ec1a35b25b75e10e7560a2a1d3a5a0260
kociumbas <kociumbas@outlook.com>
2016-02-16 01:11:03 +0100

    CSWielomian3goStopnia

---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Problem2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            textBox6.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" || textBox2.Text == "" || textBox3.Text == "" ||
+                textBox4.Text == "" || textBox5.Text == "")
+                return;
+            try
+            {
+            double wynik = 0;
+            double a = Convert.ToDouble(textBox1.Text);
+            double b = Convert.ToDouble(textBox2.Text);
+            double c = Convert.ToDouble(textBox3.Text);
+            double d = Convert.ToDouble(textBox4.Text);
+            double x = Convert.ToDouble(textBox5.Text);
+
+            
+            
+                wynik = a * x + b;
+                wynik = wynik * x + c;
+                wynik = wynik * x + d;
+
+                textBox6.Text = wynik.ToString();
+            }
+            catch (System.FormatException)
+            {
+                MessageBox.Show("Weź no, kolego, cyfry mi tu wpisuj.");
+            }
+        }
+    }
+}
